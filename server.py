f76aa6b3ccf709646e31bab87bc37f405f7691a2
Andrew Gordon <andrew.w.gordon@btinternet.com>
2023-05-06 09:11:39 +0100

    Change socket

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ async def hello(websocket):
     print(f">>> {greeting}")
 
 async def main():
-    async with websockets.serve(hello, "localhost", 8765):
+    async with websockets.serve(hello, "localhost", 8501):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
